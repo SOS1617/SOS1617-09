5c5de7361df660b2d85ef84a47ce0a0dfffd4284
sos161709mjtr <mjtr1995@gmail.com>
2017-03-21 18:53:11 +0000

    API Manuel Ready

--- index.js
+++ index.js
@@ -31,63 +31,21 @@ app.listen(port, ()=> {
 
 var routeManuel = "/api/v1/hiv-stats";
 
+var metodosManuel = require("./public/API/ApiManuel.js");
 
-//Creación de los datos
+app.get(routeManuel + "/loadInitialData",metodosManuel.getCreateStats);
+app.get(routeManuel,metodosManuel.getObtainStats);
+app.get(routeManuel + "/:name",metodosManuel.getData);
 
-app.get(routeManuel + "/loadInitialData" ,(req,res) =>{
-    
-    
- datosManuel = [   
-{ "country" : "estonia" , 	"year" : "2013"	,"incidence" : "325" ,	"total" : "8702"	, "percentage" : "24,6"},
-{"country" : "latvia" , 	"year" : "2013"	,"incidence" : "340" ,	"total" : "5867"	, "percentage" : "16,8"},
-{"country" : "portugal" , 	"year" : "2013"	,"incidence" : "1093" ,	"total" : "47390"	, "percentage" : "10,4"},
-{"country" : "belgium" , 	"year" : "2013"	,"incidence" : "1115" ,	"total" : "266850"	, "percentage" : "10,0"}
-];
-    console.log("Datos creados con el get initialData");
-    // aqui un insert a la base de datos mongoDB
- res.sendStatus(200);
-    
-});
+app.post(routeManuel,metodosManuel.postNewData);
+app.post(routeManuel + "/:name",metodosManuel.badpost);
 
+app.put(routeManuel , metodosManuel.badPut);
+app.put(routeManuel + "/:name", metodosManuel.putData);
 
-//GET a coleccion de datos
+app.delete(routeManuel,metodosManuel.deleteCollection);
+app.delete(routeManuel + "/:country" , metodosManuel.deleteData);
 
-app.get(routeManuel, (req,res) => {
-    
-   if(datosManuel.length === 0){
-       
-       res.sendStatus(404);
-       console.log("no hay ningún dato creado");
-   }else{
-   
-    res.send(datosManuel);
-    console.log("Get a coleccion realizado");
-    
-    res.sendStatus(200);
-   }
-    
-});
-
-
-
-//GET a un recurso en concreto 
-
-app.get(routeManuel + "/:name",(req,res)=> {
-    
-    var nombre = req.params.name; 
-    
-    if(nombre.length === 0){
-        res.sendStatus(400); //Petición mal hecha
-        console.log("algo para con el parámetro de búsqueda que le he pasado");
-    }else{
-        
-       var resultado =  datosManuel.filter(datosManuel.country === nombre);
-       res.send(resultado);
-       res.sendStatus(200);
-        
-    }
-    
-});
 
 
 
