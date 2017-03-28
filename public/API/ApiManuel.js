3726f35679efb30244542c59c40be81bef7edd1c
sos161709mjtr <mjtr1995@gmail.com>
2017-03-28 09:25:16 +0000

    cambios en el loadInitialData

--- public/API/ApiManuel.js
+++ public/API/ApiManuel.js
@@ -31,7 +31,7 @@ module.exports.getCreateStats = (req,res) => {
         } else {
             
             if(conjunto.length !== 0){ //Si mi base de datos está ya vacía
-                res.sendStatus(200);//OK,la base de datos ya contenía datos,por lo que ya está inicializada
+                res.sendStatus(409);//Conflicto,la base de datos ya estaba inicializada
                 
             }else{
      db.insert([
