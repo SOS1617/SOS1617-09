9c18214c605c305049df922f9a6c07d9f1d56f92
sos161709lpgc <lpontegc@gmail.com>
2017-05-10 12:14:48 +0000

    grupo

--- public/sos-09-app.js
+++ public/sos-09-app.js
@@ -9,6 +9,12 @@ angular.module("manager-app",["ngRoute"]).config(function($routeProvider){
     .when("/API/ApiLuis",{
         templateUrl : "list.html",
         controller : "ListController"
+    }).when("API/ApiManuel",{
+        templateUrl : "index.html",
+        controller : "hiv-ctrl.js"
+    }).when("API/ApiVero",{
+        templateUrl : "internetStats.html",
+        controller : "list-cntrl-internet.js"
     });
     
      console.log("APP INIT");
