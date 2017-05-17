5cdd7e5d0f6cec7f69d88d9c7eada7f68546b974
sos161709vvp <vnyka_29@hotmail.com>
2017-05-17 11:32:13 +0000

    kj

--- public/analytics/internetstats/integration.js
+++ public/analytics/internetstats/integration.js
@@ -13,79 +13,81 @@ angular
         $http
             .get("api/v2/internetandphones-stats=apikey=internetstats")
             .then(function(res) {
-                dato1 = funciondatos();
-                total.push(dato1);
-                $http
-                    .get("/proxy/internetstats")
-                    .then(function(res) {
-                        dato2 = funciondatos2();
-                        total.push(dato2);
+                dato2 = funciondatos2();
+                total.push(dato2);
+                
+                function funciondatos2() {
+                    var ret = [];
 
-                        Highcharts.chart('container01', {
-                            chart: {
-                                type: 'column',
-
-                            },
-                            title: {
-                                text: 'Highcharts'
-                            },
-                            subtitle: {
-                                text: 'Comparason male umployments and usage of internet'
-                            },
-                            plotOptions: {
-                                column: {
-                                    depth: 25
-                                }
-                            },
-                            xAxis: {
-                                categories: dato2.map(function(d) {
-                                    return d.country;
-                                })
-                            },
-                            yAxis: {
-                                title: {
-                                    text: null
-                                }
-                            },
-                            series: [{
-                                name: 'Male Uneployment (%)',
-                                data: dato1.map(function(d) {
-                                    return Number(d.male_unemployment_ratio);
-                                })
-                            }, {
-                                name: 'Usage Internet',
-                                data: dato2.map(function(d) {
-                                    return Number(d.usageinternet);
-                                })
-                            }]
+                    res.data.forEach(function(d) {
+                        res.data.country = d.country;
+                        res.data.year = d.year;
+                        res.data.usageinternet = d.usageinternet;
+                        res.data.usagephoneline = d.usagephoneline;
+                        ret.push({
+                            "country": res.data.country,
+                            "year": res.data.year,
+                            "usageinternet": res.data.usageinternet,
+                            "usagephoneline": res.data.usagephoneline
                         });
 
-
-
-
-
-
-                        function funciondatos2() {
-                            var ret = [];
-
-                            res.data.forEach(function(d) {
-                                res.data.country = d.country;
-                                res.data.year = d.year;
-                                res.data.usageinternet = d.usageinternet;
-                                res.data.usagephoneline = d.usagephoneline;
-                                ret.push({
-                                    "country": res.data.country,
-                                    "year": res.data.year,
-                                    "usageinternet": res.data.usageinternet,
-                                    "usagephoneline": res.data.usagephoneline
-                                });
-
-                            });
-
-                            return ret;
-
-                        }
                     });
+
+                    return ret;
+
+                }
+            });
+
+
+        $http
+            .get("/proxy/internetstats")
+            .then(function(res) {
+                dato1 = funciondatos();
+                total.push(dato1);
+
+                Highcharts.chart('container01', {
+                    chart: {
+                        type: 'column',
+
+                    },
+                    title: {
+                        text: 'Highcharts'
+                    },
+                    subtitle: {
+                        text: 'Comparason male umployments and usage of internet'
+                    },
+                    plotOptions: {
+                        column: {
+                            depth: 25
+                        }
+                    },
+                    xAxis: {
+                        categories: dato2.map(function(d) {
+                            return d.country;
+                        })
+                    },
+                    yAxis: {
+                        title: {
+                            text: null
+                        }
+                    },
+                    series: [{
+                        name: 'Male Uneployment (%)',
+                        data: dato1.map(function(d) {
+                            return Number(d.male_unemployment_ratio);
+                        })
+                    }, {
+                        name: 'Usage Internet',
+                        data: dato2.map(function(d) {
+                            return Number(d.usageinternet);
+                        })
+                    }]
+                });
+
+
+
+
+
 
                 function funciondatos() {
                     var ret = [];
@@ -109,5 +111,10 @@ angular
                 }
 
 
+                
             });
+
+
+
+
     }]);
