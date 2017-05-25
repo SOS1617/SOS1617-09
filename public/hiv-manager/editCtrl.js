b6722f1251ec8eadab6aea1ae2ff1228a2b29650
sos161709mjtr <mjtr1995@gmail.com>
2017-05-25 01:10:07 +0000

    cambios realizados en mi visualización

--- public/hiv-manager/editCtrl.js
+++ public/hiv-manager/editCtrl.js
@@ -3,12 +3,12 @@ angular
 .module("sos09-app")
 .controller("editCtrl",["$scope","$http","$routeParams","$location",function($scope,$http,$routeParams,$location){
     console.log("Edit controller initialized");
-    var url = "https://sos1617-09.herokuapp.com/api/v2";
+    $scope.url  = "https://sos1617-09.herokuapp.com/api/v2/hiv-stats/";
     var apikey = "apikey=manuel";
     
     function refresh(){
         $http
-            .get(url + "/hiv-stats/" + $routeParams.name + "/" + Number($routeParams.year) + "?" + apikey)
+            .get($scope.url + $routeParams.name + "/" + Number($routeParams.year) + "?" + apikey)
             .then(function successCallback(response) {
                     $scope.updateData = response.data[0];
 
@@ -22,7 +22,7 @@ angular
     $scope.updatedData = function(newData){
       
             $http
-                .put($scope.url + "/" + newData.name + "/" + Number(newData.year) + "?apikey=manuel", {
+                .put($scope.url  + newData.country + "/" + Number(newData.year) + "?apikey=manuel", {
                     country: newData.country,
                     year: newData.year,
                     incidence: newData.incidence,
--- public/sos-09-app.js
+++ public/sos-09-app.js
@@ -18,14 +18,6 @@ angular.module("sos09-app",["ngRoute"]).config(function($routeProvider){
         templateUrl : "/ticsathome-manager/edit.html",
         controller : "EditController"
 
-    }).when("/hiv-stats",{
-        templateUrl : "/API/ApiManuel/list.html",
-        controller : "listCtrl"
-   
-    }).when("/hiv-stats/edit/:name/:year",{
-        templateUrl : "/API/ApiManuel/edit.html",
-        controller : "editCtrl"
-  
     }).when("API/ApiVero",{
         templateUrl : "internetStats.html",
         controller : "list-cntrl-internet.js"
